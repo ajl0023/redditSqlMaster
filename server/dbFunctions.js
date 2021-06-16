22265f63d959279f2f107c0b600c0e5dd8943f5e
austin <austinlee199723@yahoo.com>
2021-06-15 18:11:11 -0700

    fixed aws s3

--- server/dbFunctions.js
+++ server/dbFunctions.js
@@ -2,11 +2,12 @@ const mysql = require("mysql");
 const con = mysql.createConnection({
   host: "192.168.0.249",
   user: "newuser",
+  port: "3306",
   password: "100962Austin",
   database: "mydb",
   multipleStatements: true,
 });
-console.log(con);
+
 con.connect(function (err) {
   if (err) throw err;
 });
