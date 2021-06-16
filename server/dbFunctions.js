9fcb7f875faac8cadac3c039bf76673891dae0c2
austin <austinlee199723@yahoo.com>
2021-06-15 20:14:11 -0700

    serverless mysql

--- server/dbFunctions.js
+++ server/dbFunctions.js
@@ -1,16 +1,15 @@
 const mysql = require("mysql");
-const con = require("serverless-mysql")({
-  config: {
-    host: "192.168.0.249",
-    user: "newuser",
-    port: 3306,
-    password: "1",
-    database: "mydb",
-    connectTimeout: 30000,
-    multipleStatements: true,
-  },
-});
+const con = require("serverless-mysql")();
 
+con.config({
+  host: "192.168.0.249",
+  user: "newuser",
+  port: 3306,
+  password: "1",
+  database: "mydb",
+  connectTimeout: 30000,
+  multipleStatements: true,
+});
 con.connect(function (err) {
   console.log("🚀 ~ file: dbFunctions.js ~ line 12 ~ err", err);
   console.log(err);
