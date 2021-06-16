0f836a3992c2cdabad5f6a1f26a7215d1c78e3ff
austin <austinlee199723@yahoo.com>
2021-06-16 12:01:26 -0700

    serverless mysql

--- server/dbFunctions.js
+++ server/dbFunctions.js
@@ -2,7 +2,8 @@ module.exports = {
   connect: () => {
     const mysql = require("mysql");
     const con = mysql.createPool({
-      host: "192.168.0.249",
+      host: "127.0.0.1",
+      port: 3306,
       user: "testuser",
       password: "test",
       database: "mydb",
