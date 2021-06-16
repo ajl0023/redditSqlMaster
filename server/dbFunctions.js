5225df56d685d733204039d455824bbfbc53b689
austin <austinlee199723@yahoo.com>
2021-06-16 12:41:50 -0700

    serverless mysql

--- server/dbFunctions.js
+++ server/dbFunctions.js
@@ -4,8 +4,8 @@ module.exports = {
     const con = mysql.createPool({
       host: "192.168.0.249",
       port: 3306,
-      user: "newuser",
-      password: "1234",
+      user: "verceltest",
+      password: "",
       database: "mydb",
       multipleStatements: true,
     });
