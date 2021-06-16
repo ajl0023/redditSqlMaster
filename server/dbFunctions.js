6267ee9766b11d9413f2b913f324115f088fac5a
austin <austinlee199723@yahoo.com>
2021-06-16 13:10:43 -0700

    serverless mysql

--- server/dbFunctions.js
+++ server/dbFunctions.js
@@ -5,7 +5,7 @@ module.exports = {
       host: "192.168.0.249",
       port: 3306,
       user: "verceltest",
-      password: "",
+      password: "1",
       database: "mydb",
       multipleStatements: true,
     });
