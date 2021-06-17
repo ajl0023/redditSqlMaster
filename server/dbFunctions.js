442a6f6669e8a87aac25b1865c20a9f7628b4005
austin <austinlee199723@yahoo.com>
2021-06-16 17:57:12 -0700

    serverless mysql

--- server/dbFunctions.js
+++ server/dbFunctions.js
@@ -4,7 +4,7 @@ module.exports = {
     const con = mysql.createPool({
       host: "database-1.c0yjxjsy2dww.us-west-1.rds.amazonaws.com",
       port: 3306,
-
+      user: "admin",
       password: "100962austin",
       database: "readditdb",
       multipleStatements: true,
